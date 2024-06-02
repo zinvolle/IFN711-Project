8c05d6c04637679cb6d832b849082caa0f0b3bfc
AlexM <alexander-moyes@hotmail.com>
2024-06-02 14:10:19 +1000

    update packages

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ViewBlocks from './components/ViewBlocks';
 import EmployerPage from './components/EmployerReceiving';
 import EncryptAndDecrypt from './components/EncryptDecrypt';
-import ContractInformation from './components/contractInformation'
+import ContractInformation from './components/contractInformation';
+import Helia from './components/helia.js';
 import IPFS from './components/ipfs'
 import {IPFSUploader,IPFSDownloader} from './components/ipfs';
 //import IPFSDownloader from './components/ipfs';
